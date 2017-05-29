b68d5269650d833719939fd6402811e4662cf69c
vivalavida20 <vivalavida2051@gmail.com>
2017-05-30 07:13:15 +0900

    url에 많은 인자를 한꺼번에 받는 방법

--- dojo/views.py
+++ dojo/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 # x 또는 y값이 들어오지 않을 때 기본 값을 지정
-def mysum(request, x=0, y=0, z=0):
-    # request : HttpRequest
-    return HttpResponse(int(x) + int(y))
+def mysum(request, numbers):
+
+    result = sum(map(int, numbers.split("/")))
+    return HttpResponse(result)
