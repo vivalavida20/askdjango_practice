299f3fade84ae5db63d667ec2dc2ef7a3483b956
vivalavida20 <vivalavida2051@gmail.com>
2017-05-30 07:21:21 +0900

    dojo.views.hello : url 한글 문자열 매칭

--- dojo/views.py
+++ dojo/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-# x 또는 y값이 들어오지 않을 때 기본 값을 지정
+# x 또는 y값이 들어오지 않을 때 기본 값을 지
 def mysum(request, numbers):
 
     result = sum(map(int, numbers.split("/")))
     return HttpResponse(result)
+
+def hello(request, name, age):
+
+    return HttpResponse(
+        "안녕하세요. {}입니다. 저는 {}살입니다.".format(name, age)
+    )
