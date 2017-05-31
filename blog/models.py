319a8bc39f47c68b1dedc0a00c34b23f93b8e5a1
vivalavida20 <vivalavida2051@gmail.com>
2017-06-01 07:01:49 +0900

    모델 변경

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+import re
+from django.forms import ValidationError
 
-# Create your models here.
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name="제목", help_text="포스팅 제목을 입력해주세요 최대 100자") #verbose_name을 설정하면 브라우저별 언어 설정에 대응하기가 어렵다는 단점
     content = models.TextField()
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, blank=True, help_text="경도,위도 포맷으로 입력",
+                              validators=[lnglat_validator],)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # auto_now_add - 최초 생성 시 값이 생성 auto_now - 조회할 때마다 값이 갱신
