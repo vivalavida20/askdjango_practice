902fc697997d29da1a1835d0ee4d8e6551f64d5a
vivalavida20 <vivalavida2051@gmail.com>
2017-06-02 23:23:58 +0900

    일일커밋용

--- blog/models.py
+++ blog/models.py
@@ -15,4 +15,4 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # auto_now_add - 최초 생성 시 값이 생성 auto_now - 조회할 때마다 값이 갱신
-
+    # 일단 커밋용
