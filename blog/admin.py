64c5ea3a5f74e08ed97fbfe872fd15f8f182583d
vivalavida20 <vivalavida2051@gmail.com>
2017-06-09 00:21:47 +0900

    mark_safe 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from .models import Post
+from django.utils.safestring import mark_safe
 # Register your models here.
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-	list_display = ['id', 'title', 'created_at', 'updated_at']
+	# list_display할 수 있는 요소는 필드명/속성명/함수명(인자없는)
+	list_display = ['id', 'title', 'content_length','created_at', 'updated_at']
+
+
+	def content_length(self, post):
+		return mark_safe("<strong>{}글자</strong>".format(len(post.content)))
+
+	# admin에 노출하고 싶은 컬럼명
+	content_length.short_description = '글자수'
 
 #admin.site.register(Post, PostAdmin)
