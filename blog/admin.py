ebc3e94fb162c062634b688cab470e402e356d55
vivalavida20 <vivalavida2051@gmail.com>
2017-06-09 00:32:46 +0900

    blog.models.Post 모델에 status 필드를 추가하고, 이를 처리하는 Custom Admin Action 구현

--- blog/admin.py
+++ blog/admin.py
@@ -6,8 +6,8 @@ from django.utils.safestring import mark_safe
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
 	# list_display할 수 있는 요소는 필드명/속성명/함수명(인자없는)
-	list_display = ['id', 'title', 'content_length','created_at', 'updated_at']
-
+	list_display = ['id', 'title', 'status', 'content_length','created_at', 'updated_at']
+	actions = ['make_published']
 
 	def content_length(self, post):
 		return mark_safe("<strong>{}글자</strong>".format(len(post.content)))
@@ -15,4 +15,8 @@ class PostAdmin(admin.ModelAdmin):
 	# admin에 노출하고 싶은 컬럼명
 	content_length.short_description = '글자수'
 
+	def make_published(self, request, queryset):
+		updated_count = queryset.update(status='p')
+		self.message_user(request, "{}건의 포스팅을 공개했습니다.".format(updated_count))
+	make_published.short_description = "포스팅을 바로 출판합니다."
 #admin.site.register(Post, PostAdmin)
