2096fd2295fb0b63b57121bc80e44076c4e75188
vivalavida20 <vivalavida2051@gmail.com>
2017-07-01 16:31:44 +0900

    accounts app admin에 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
+#accounts/models.py
 from django.db import models
-
+from django.contrib.auth.models import User 
 # Create your models here.
+class Profile(models.Model):
+	# user = models.ForeignKey(User) # 이건 배드케이스라고 함!!
+	user = models.OneToOneField(User)
+	phone_number = models.CharField(max_length=20)
+	address = models.CharField(max_length=50) 
