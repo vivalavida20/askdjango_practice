2b17fc061edd2bee0be1ac576eb37c3dadf67558
vivalavida20 <vivalavida2051@gmail.com>
2017-07-01 17:02:31 +0900

    settings.AUTH_USER_MODEL의 올바른 지정

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,11 @@
 #accounts/models.py
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User 
 # Create your models here.
 class Profile(models.Model):
 	# user = models.ForeignKey(User) # 이건 배드케이스라고 함!!
-	user = models.OneToOneField(User)
+	# user = models.OneToOneField(User)
+	user = models.OneToOneField(settings.AUTH_USER_MODEL)
 	phone_number = models.CharField(max_length=20)
 	address = models.CharField(max_length=50) 
